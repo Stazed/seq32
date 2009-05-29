271bf3abc3ddc3008168986a54f55d9a31ef5802
Guido Scholz <guido.scholz@bayernline.de>
2009-05-29 22:01:35 +0200

    Documentation for mute groups added (provided by Andrea delle Canne and Frank Kober)

--- src/event.h
+++ src/event.h
@@ -37,8 +37,9 @@ const unsigned char  EVENT_CLEAR_CHAN_MASK  = 0xF0;
 const unsigned char  EVENT_MIDI_SONG_POS    = 0xF2;
 const unsigned char  EVENT_MIDI_CLOCK       = 0xF8;
 const unsigned char  EVENT_MIDI_START       = 0xFA;
-const unsigned char  EVENT_MIDI_STOP        = 0xFC;
 const unsigned char  EVENT_MIDI_CONTINUE    = 0xFB;
+const unsigned char  EVENT_MIDI_STOP        = 0xFC;
+
 const unsigned char  EVENT_SYSEX            = 0xF0;
 const unsigned char  EVENT_SYSEX_END        = 0xF7;
 
