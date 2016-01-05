23e09b182433888ed9fa1ecaff67e8132a9521aa
Stazed <Stazed@mapson.com>
2016-01-04 20:15:28 -0600

    fix slave mode with no master(bbt) remove useless pbeat calc

--- src/perform.cpp
+++ src/perform.cpp
@@ -1574,6 +1574,15 @@ void perform::output_func(void)
                 m_jack_transport_state = jack_transport_query( m_jack_client, &m_jack_pos );
                 m_jack_frame_current =  jack_get_current_transport_frame( m_jack_client );
 
+                if( (uint32_t)m_jack_pos.beats_per_bar == 0 ) // to fix the case of no master set while in slave mode
+                {
+                    // in this case the bbt info will be 0 so just plug it so it will play
+                    m_jack_pos.beats_per_bar = m_bp_measure;
+                    m_jack_pos.beat_type = 4;
+                    m_jack_pos.ticks_per_beat = c_ppqn * 10;
+                    m_jack_pos.beats_per_minute =  m_master_bus.get_bpm();
+                }
+
                 if ( m_jack_transport_state_last  ==  JackTransportStarting &&
                         m_jack_transport_state       == JackTransportRolling ){
 
@@ -1686,8 +1695,8 @@ void perform::output_func(void)
 
 
 
-                    long pbeat = (long) ((long) m_jack_tick % (long) (m_jack_pos.ticks_per_beat *  m_jack_pos.beats_per_bar ));
-                    pbeat = pbeat / (long) m_jack_pos.ticks_per_beat;
+                    //long pbeat = (long) ((long) m_jack_tick % (long) (m_jack_pos.ticks_per_beat *  m_jack_pos.beats_per_bar ));
+                    //pbeat = pbeat / (long) m_jack_pos.ticks_per_beat;
 
                     //long ptick = (long) m_jack_tick % (long) m_jack_pos.ticks_per_beat;
                     //long pbar  = (long) ((long) m_jack_tick / (m_jack_pos.ticks_per_beat *  m_jack_pos.beats_per_bar ));
@@ -2374,6 +2383,7 @@ void perform::set_key_group( unsigned int keycode, long group_slot )
 
 
 #ifdef JACK_SUPPORT
+
 void jack_timebase_callback(jack_transport_state_t state,
         jack_nframes_t nframes,
         jack_position_t *pos, int new_pos, void *arg)
@@ -2443,7 +2453,6 @@ void jack_timebase_callback(jack_transport_state_t state,
 
     state_last = state_current;
 }
-
 
 void jack_shutdown(void *arg)
 {
