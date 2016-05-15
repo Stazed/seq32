85ceb67db12f61af37df2f8b2f642bc43ee7525d
Stazed <Stazed@mapson.com>
2016-05-15 11:59:07 -0500

    Fix seqevent selection color.

--- src/seqevent.cpp
+++ src/seqevent.cpp
@@ -406,7 +406,7 @@ seqevent::draw_selection_on_window()
         m_old.x = x;
         m_old.width = w;
 
-        m_gc->set_foreground(m_dk_blue);
+        m_gc->set_foreground(m_red);
         m_window->draw_rectangle(m_gc,false,
                                  x,
                                  y,
@@ -421,7 +421,7 @@ seqevent::draw_selection_on_window()
         x = m_selected.x + delta_x;
         x -= m_scroll_offset_x;
 
-        m_gc->set_foreground(m_dk_blue);
+        m_gc->set_foreground(m_red);
         m_window->draw_rectangle(m_gc,false,
                                  x,
                                  y,
