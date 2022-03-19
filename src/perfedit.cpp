409b30fc3fb4c3e0ab1eaaae3c3e2135be012e76
Stazed <stazed@manjo-mapson.com>
2022-03-19 14:57:28 -0500

    Fix expand and collapse regression again.

--- src/perfedit.cpp
+++ src/perfedit.cpp
@@ -673,7 +673,7 @@ void
 perfedit::collapse()
 {
     m_mainperf->push_trigger_undo(-1); //  all tracks
-    m_mainperf->move_triggers( false, m_mainperf->get_right_tick() );
+    m_mainperf->move_triggers( false, m_mainperf->get_left_tick() );
     m_perfroll->redraw_all_tracks();
 }
 
